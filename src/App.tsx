8e094dc84c0da149d3203b25d47d8486beb30b53
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-01-23 10:14:06 +0100

    fetch data and save to state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react"
+
 
 function App() {
+
+  useEffect(() => {
+    const url = "https://cryptoyellbinanceorderhis.herokuapp.com/api/my-trades-performance/BTCUSDT"
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        setTrades(data)
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const [trades, setTrades] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Binance order history</h1>
     </div>
   );
 }
