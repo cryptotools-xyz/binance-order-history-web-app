6a85f70ac3adad670f7f6809955b689548cdcf53
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-01-24 00:24:48 +0100

    Add the ability to sort the column in the table #8
    * Code refactor into a Table component
    
    * Sorting by the cost
    
    * Add buttons to sort by column
    
    * Sort in descending order

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from "react"
 import 'bootstrap/dist/css/bootstrap.css';
+import Table from './components/Table';
 
 function App() {
 
@@ -11,8 +12,7 @@ function App() {
       try {
         const response = await fetch(url);
         const data = await response.json();
-        console.log(data);
-        setTrades(data)
+        setTrades(data);
       } catch (error) {
         console.log("error", error);
       }
@@ -26,40 +26,7 @@ function App() {
   return (
     <div>
       <h1>Binance order history</h1>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">orderId</th>
-            <th scope="col">time</th>
-            <th scope="col">symbol</th>
-            <th scope="col">qty</th>
-            <th scope="col">price</th>
-
-            <th scope="col">cost</th>
-            <th scope="col">worth</th>
-            <th scope="col">profit</th>
-            <th scope="col">percentage_change</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            trades.map((item: any) => {
-              console.log(item)
-              return <tr>
-                <td >{item.orderId}</td>
-                <td>{item.time}</td>
-                <td>{item.symbol}</td>
-                <td>{item.qty}</td>
-                <td>{item.price}</td>
-                <td>{item.performance.cost}</td>
-                <td>{item.performance.worth}</td>
-                <td>{item.performance.profit}</td>
-                <td>{item.performance.percentage_change}</td>
-              </tr>
-            })
-          }
-        </tbody>
-      </table>
+      <Table trades={trades} />
     </div>
   );
 }
---
+++ src/components/Table.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react"
+import _ from 'lodash';
+
+function Table(props: any) {
+    const { trades } = props;
+    let sortedTrades = [...trades];
+
+    const [sortedField, setSortedField] = React.useState("cost");
+
+    if (sortedField !== null) {
+        sortedTrades.sort((a, b) => {
+            if (_.get(a, sortedField) > _.get(b, sortedField)) {
+                return -1;
+            }
+            if (_.get(a, sortedField) < _.get(b, sortedField)) {
+                return 1;
+            }
+            return 0;
+        });
+    }
+
+    return (
+        <table className="table">
+            <thead>
+                <tr>
+                    <th scope="col">orderId</th>
+                    <th scope="col">
+                        <button type="button" onClick={() => setSortedField('time')}>
+                            time
+                        </button>
+                    </th>
+                    <th scope="col">symbol</th>
+                    <th scope="col">
+                        <button type="button" onClick={() => setSortedField('qty')}>
+                            qty
+                        </button>
+                    </th>
+                    <th scope="col">
+                        <button type="button" onClick={() => setSortedField('price')}>
+                            price
+                        </button>
+                    </th>
+
+                    <th scope="col">
+                        <button type="button" onClick={() => setSortedField('performance.cost')}>
+                            cost
+                        </button>
+                    </th>
+                    <th scope="col">
+                        <button type="button" onClick={() => setSortedField('performance.worth')}>
+                            worth
+                        </button>
+                    </th>
+                    <th scope="col">
+                        <button type="button" onClick={() => setSortedField('performance.profit')}>
+                            profit
+                        </button>
+                    </th>
+                    <th scope="col">
+                        <button type="button" onClick={() => setSortedField('performance.percentage_change')}>
+                            percentage_change
+                        </button>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    sortedTrades.map((item: any, index: number) => {
+                        return <tr key={index}>
+                            <td >{item.orderId}</td>
+                            <td>{item.time}</td>
+                            <td>{item.symbol}</td>
+                            <td>{item.qty}</td>
+                            <td>{item.price}</td>
+                            <td>{item.performance.cost}</td>
+                            <td>{item.performance.worth}</td>
+                            <td>{item.performance.profit}</td>
+                            <td>{item.performance.percentage_change}</td>
+                        </tr>
+                    })
+                }
+            </tbody>
+        </table>
+    );
+}
+
+export default Table;
