3be831d8aa1631bb578d23d45a4aa23d1f70e259
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-02-01 21:29:40 +0100

    Loading state

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,16 @@ function App() {
       throw new Error("The REACT_APP_WEB_API_URL variable is not defined.");
     }
 
+    setLoading(true);
     const fetchData = async () => {
       try {
         const response = await fetch(url);
         const data = await response.json();
         setTrades(data);
+        setLoading(false);
       } catch (error) {
         console.log("error", error);
+        setLoading(false);
       }
     };
 
@@ -27,22 +30,26 @@ function App() {
   }, []);
 
   const [trades, setTrades] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   return (
     <div>
       <h1>Binance order history</h1>
 
-      <ul>
-        <li>qty sum: {trades.reduce((sum, { qty, order: { side } }) => {
-          if(side === "BUY") return sum + parseFloat(qty)
-          if(side === "SELL") return sum - parseFloat(qty)
-          return 0;
-        }, 0)}</li>
-      </ul>
+      {loading ? <p>We're loading, please wait.</p> : <>
+        <ul>
+          <li>qty sum: {trades.reduce((sum, { qty, order: { side } }) => {
+            if (side === "BUY") return sum + parseFloat(qty)
+            if (side === "SELL") return sum - parseFloat(qty)
+            return 0;
+          }, 0)}</li>
+        </ul>
 
-      <hr />
+        <hr />
 
-      <Table trades={trades} />
+        <Table trades={trades} />
+      </>}
+
     </div>
   );
 }
